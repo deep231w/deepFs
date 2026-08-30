d0089e783fdc1b9914294e435d03592520780357
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-08-31 05:17:15 +0530

    added chunk top save to file

--- internal/engine/chunkingFIle.go
+++ internal/engine/chunkingFIle.go
@@ -4,11 +4,18 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 )
 
 
 func fileChunking(file *os.File) error {
 	buffer:= make([]byte , 5*1024*1024) //5mb
+	c:=0
+	outputFolder:="../../uploads/chunks"
+	err := os.MkdirAll(outputFolder, 0755)
+	if err != nil {
+		panic(err)
+	}
 
 	for{
 		n, err := file.Read(buffer)
@@ -19,9 +26,17 @@ func fileChunking(file *os.File) error {
 			}
 			return  err
 		}
+		// fmt.Println(string(buffer[:n]))
 
-		fmt.Println(buffer[:n])
-
+		fileName:= fmt.Sprintf("chunk_%d", c)
+		fullpath:=filepath.Join(outputFolder, fileName)
+		// file, err:= os.Create(fileName)
+		c++
+		err= os.WriteFile(fullpath , buffer[:n],0644)
+		if err!= nil {
+			return err
+		}
+		fmt.Printf("file created %v", fullpath)
 	}
 
 	return nil
