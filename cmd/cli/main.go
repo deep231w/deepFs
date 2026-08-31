90e83b03fb91d06900e098adf860587aa50adafb
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-08-31 09:31:23 +0530

    enhanced cli by adding commands

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -6,15 +6,13 @@ import (
 	"github.com/DeepSystems/deepfs/internal/engine"
 )
 
-func main() {
-	fmt.Println("DeepFS CLI")
-	
+func upload(){
 	if len(os.Args) < 2{
 		fmt.Println("Please provide a file path")
 		return 
 	}
 
-	filepath := os.Args[1];
+	filepath := os.Args[2];
 	
 	e:= engine.New()
 
@@ -22,5 +20,24 @@ func main() {
 
 	if err != nil{
 		fmt.Println(err);
+	}
+
+}
+
+func play(){
+	fmt.Println("Playing ......")
+}
+func main() {
+	fmt.Println("DeepFS CLI")
+	arg:=os.Args[1]
+	fmt.Printf("args = %v\n", arg)
+	
+	switch arg{
+	case "upload":
+		upload()
+	case "play":
+		play()
+	default:
+		fmt.Println("Command doesnt exist : ", arg)
 	}
 }
