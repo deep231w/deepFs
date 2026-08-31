49eb97ba804b4babe6d5dea1e80164abe28ff489
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-08-31 10:42:45 +0530

    merging chunks to file added

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+
 	"github.com/DeepSystems/deepfs/internal/engine"
 )
 
@@ -24,9 +26,37 @@ func upload(){
 
 }
 
-func play(){
-	fmt.Println("Playing ......")
+func play() error{
+	fmt.Println("Converting to file ......")
+	output, err := os.Create("../../uploads/reconstructed.mp4")
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+	for i := 0; ; i++ {
+		path := filepath.Join(
+			"../../uploads/chunks",
+			fmt.Sprintf("chunk_%d", i),
+		)
+
+		chunk, err := os.ReadFile(path)
+
+		if err != nil {
+			if os.IsNotExist(err) {
+				break
+			}
+			return err
+		}
+
+		_, err = output.Write(chunk)
+		if err != nil {
+			return err
+		}
+	}
+	fmt.Println("Converted successfully to file !!!")
+	return nil
 }
+
 func main() {
 	fmt.Println("DeepFS CLI")
 	arg:=os.Args[1]
