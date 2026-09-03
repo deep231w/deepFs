515d413ee698a8a97f41cdf5a070d7cb15231db7
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-09-03 09:16:01 +0530

    update: added routes

---
+++ internal/api/router.go
@@ -0,0 +1,14 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/DeepSystems/deepfs/internal/api/routes"
+)
+
+func Router() *http.ServeMux{
+	mux:= http.NewServeMux();
+
+	routes.RegisterUsersRoute(mux)
+	return mux
+}
---
+++ internal/api/routes/users.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/DeepSystems/deepfs/internal/api/handlers"
+)
+
+func RegisterUsersRoute(mux *http.ServeMux){
+	mux.HandleFunc("GET users",handlers.GetUsers)
+}
