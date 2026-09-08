522b1df3f08f25239c11a0c143707c9c46bfe8f8
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-09-08 14:44:19 +0530

    fix :Added random state genaration instead of static

--- internal/api/handlers/auth.go
+++ internal/api/handlers/auth.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -10,18 +12,48 @@ import (
 	"github.com/DeepSystems/deepfs/internal/api/config"
 )
 
+func generateRandomState(len int)(string, error){
+	b:= make([]byte, len)
+	_ , err:= rand.Read(b)
+	if err != nil {
+		return "", nil
+	}
+
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
 func GoogleLogin(w http.ResponseWriter , r * http.Request){
-	url:= config.AppConfig.GoogleLoginConfig.AuthCodeURL("randomstate")
+	rstate, err := generateRandomState(32)
+	if err != nil {
+		http.Error(w, "State generation faoiled!!", http.StatusInternalServerError)
+	}
+
+	// save crypto state in httponly cookie
+	http.SetCookie(w, &http.Cookie{
+		Name: "oauth_state",
+		Value: rstate,
+		Path: "/",
+		MaxAge: 300,
+		HttpOnly: true,
+		Secure: false,
+		SameSite: http.SameSiteLaxMode,
+	})
+
+	url:= config.AppConfig.GoogleLoginConfig.AuthCodeURL(rstate)
 	fmt.Printf("app config = %v", config.AppConfig)
 	http.Redirect(w , r , url , http.StatusTemporaryRedirect)
 }
 
 func GoogleCallBack(w http.ResponseWriter, r *http.Request){
 	state:= r.URL.Query().Get("state")
-	if state != "randomstate" {
-		http.Error(w , "status didnt match !!",  http.StatusBadRequest)
+	cookie, err:= r.Cookie("state")
+	if err != nil || state != cookie.Value {
+		http.Error(w , "State mismatch or cookie expired !!",  http.StatusBadRequest)
 		return
 	}
+	// Delete state cookie right away
+	http.SetCookie(w, &http.Cookie{Name: "oauth_state", Value: "", Path: "/", MaxAge: -1})
+
 	code:= r.URL.Query().Get("code")
 	googlecon:=config.GoogleAuthConfig()
 	token, err:=googlecon.Exchange(context.Background() , code)
